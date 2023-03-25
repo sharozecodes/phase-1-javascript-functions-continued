59bd137dbbb92c73cabfdada3ee1a2609801a516
Sharoze Archer <sharoze.archer@gmail.com>
2023-03-25 00:35:54 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+
+function mondayWork(task = "go to the office"){
+
+    return `This Monday, I will ${task}.`;
+
+}
+
+function saturdayFun(fun = "roller-skate"){
+
+    return `This Saturday, I want to ${fun}!`;
+
+}
+
+
+function wrapAdjective(flair = "*"){
+
+    return function (note = "special"){
+        return `You are ${flair}${note}${flair}!`;
+    };
+
+}
